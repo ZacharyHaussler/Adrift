e320158300a0322bdb509aa126f57d594d43e097
Zachary Haussler <zach.hauss@gmail.com>
2026-02-22 00:31:14 -0600

    tested primitive movement in six directions

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-
-public Rb ridgedBody;
-
+//using UnityEngine.InputSystem;
 
 
 
 public class PlayerScript : MonoBehaviour {
 
-    private PlayerControls playerControls;
+    public Rigidbody rigidbody;
+
+    //private PlayerControls playerControls;
+    private Vector3 ThrustDirection = new Vector3(0f,0f,0f);
+    private float AngleForce = 0.0f;
+
 
     void Awake() {
-        playerControls = new PlayerControls();
-        playerControls.Player.
+        //playerControls = new PlayerControls();
+        
+
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,17 +24,19 @@ public class PlayerScript : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
-        
+    void FixedUpdate() {
+        ThrustDirection = new Vector3(0f,0f,0f);
+        ThrustDirection.x += (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
+        ThrustDirection.z += (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
+        ThrustDirection.y += (Input.GetKey(KeyCode.Space) ? 1 : 0) - (Input.GetKey(KeyCode.LeftShift) ? 1 : 0);
+        Debug.Log(ThrustDirection);
+        if (ThrustDirection != new Vector3(0f,0f,0f)) {
+            rigidbody.AddForce(2*ThrustDirection/ThrustDirection.magnitude, ForceMode.Force);
+        }
+
     }
 
-    void EnablePlayerControls(bool on) {
-        if (on) {
-            
-        }
-            
-        
-    }
+    
 
 
 }
