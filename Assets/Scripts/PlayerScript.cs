66d07231f82ba20fa1a67cfc9c6532e1e2b324b6
Zachary Haussler <zach.hauss@gmail.com>
2026-02-21 21:34:42 -0600

    check to see if collaborators can see commits

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public Rb ridgedBody;
+
+
+
+
+public class PlayerScript : MonoBehaviour {
+
+    private PlayerControls playerControls;
+
+    void Awake() {
+        playerControls = new PlayerControls();
+        playerControls.Player.
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void EnablePlayerControls(bool on) {
+        if (on) {
+            
+        }
+            
+        
+    }
+
+
+}
