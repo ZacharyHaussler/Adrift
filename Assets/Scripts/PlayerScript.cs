46de977e2df71a3aa87a02e55514905037fe95c3
Zachary Haussler <zach.hauss@gmail.com>
2026-02-22 16:20:15 -0600

    -Mouse movement added -directional movement based on player's look direction

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 //using UnityEngine.InputSystem;
 
@@ -6,8 +7,16 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     public Rigidbody rigidbody;
+    public float XSensitivity;
+    public float YSensitivity;
 
-    //private PlayerControls playerControls;
+    private float XRotation;
+    private float YRotation;
+    
+    
+
+
+    
     private Vector3 ThrustDirection = new Vector3(0f,0f,0f);
     private float AngleForce = 0.0f;
 
@@ -20,18 +29,38 @@ public class PlayerScript : MonoBehaviour {
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
     }
 
-    // Update is called once per frame
+    void LateUpdate() {
+        
+
+        XRotation = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * YSensitivity * -1;
+        YRotation = Input.GetAxisRaw("Mouse X") * Time.deltaTime * XSensitivity;
+        
+        
+
+        transform.rotation *= Quaternion.Euler(XRotation, YRotation, 0);
+
+    }
+    
+    
+    // FixedUpdate is called 50 times per second
     void FixedUpdate() {
         ThrustDirection = new Vector3(0f,0f,0f);
         ThrustDirection.x += (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
         ThrustDirection.z += (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
         ThrustDirection.y += (Input.GetKey(KeyCode.Space) ? 1 : 0) - (Input.GetKey(KeyCode.LeftShift) ? 1 : 0);
-        Debug.Log(ThrustDirection);
+        
+        
+
+
         if (ThrustDirection != new Vector3(0f,0f,0f)) {
-            rigidbody.AddForce(2*ThrustDirection/ThrustDirection.magnitude, ForceMode.Force);
+            rigidbody.AddForce(4f*ThrustDirection.x*(new Vector3(0f,0f,0f)+transform.right), ForceMode.Force);
+            rigidbody.AddForce(4f*ThrustDirection.y*(new Vector3(0f,0f,0f)+transform.up), ForceMode.Force);
+            rigidbody.AddForce(4f*ThrustDirection.z*(new Vector3(0f,0f,0f)+transform.forward), ForceMode.Force);
         }
 
     }
