320acb7d2092584abab63dc73d003b88e124f14a
Matthew Lundeen <decentishdev@gmail.com>
2026-02-23 00:38:12 -0600

    cleaning up plus bad map

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,20 +2,14 @@ using Unity.Mathematics;
 using UnityEngine;
 //using UnityEngine.InputSystem;
 
-
-
 public class PlayerScript : MonoBehaviour {
 
     public Rigidbody rigidbody;
-    public float XSensitivity;
-    public float YSensitivity;
+    public float XSensitivity = 100f;
+    public float YSensitivity = 100f;
 
     private float XRotation;
     private float YRotation;
-    
-    
-
-
     
     private Vector3 ThrustDirection = new Vector3(0f,0f,0f);
     private float AngleForce = 0.0f;
@@ -23,49 +17,37 @@ public class PlayerScript : MonoBehaviour {
 
     void Awake() {
         //playerControls = new PlayerControls();
-        
-
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
     }
 
     void LateUpdate() {
-        
 
         XRotation = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * YSensitivity * -1;
         YRotation = Input.GetAxisRaw("Mouse X") * Time.deltaTime * XSensitivity;
-        
-        
 
         transform.rotation *= Quaternion.Euler(XRotation, YRotation, 0);
 
     }
-    
-    
-    // FixedUpdate is called 50 times per second
-    void FixedUpdate() {
-        ThrustDirection = new Vector3(0f,0f,0f);
-        ThrustDirection.x += (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
-        ThrustDirection.z += (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
-        ThrustDirection.y += (Input.GetKey(KeyCode.Space) ? 1 : 0) - (Input.GetKey(KeyCode.LeftShift) ? 1 : 0);
-        
-        
 
+    void FixedUpdate(){
 
-        if (ThrustDirection != new Vector3(0f,0f,0f)) {
-            rigidbody.AddForce(4f*ThrustDirection.x*(new Vector3(0f,0f,0f)+transform.right), ForceMode.Force);
-            rigidbody.AddForce(4f*ThrustDirection.y*(new Vector3(0f,0f,0f)+transform.up), ForceMode.Force);
-            rigidbody.AddForce(4f*ThrustDirection.z*(new Vector3(0f,0f,0f)+transform.forward), ForceMode.Force);
-        }
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        float y = (Input.GetKey(KeyCode.Space) ? 1f : 0f) - (Input.GetKey(KeyCode.LeftShift) ? 1f : 0f);
 
+        Vector3 worldForce =
+            x * transform.right +
+            y * transform.up +
+            z * transform.forward;
+
+        rigidbody.AddForce(4f * worldForce.normalized, ForceMode.Force);
     }
-
-    
-
 
 }
