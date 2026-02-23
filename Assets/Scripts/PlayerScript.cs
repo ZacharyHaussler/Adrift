d452f888cb68b6f249e13f4f61d0c3cfebb50ca9
Zachary Haussler <zach.hauss@gmail.com>
2026-02-23 13:25:06 -0600

    -grapple functionality with left click

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -23,6 +23,13 @@ public class PlayerScript : MonoBehaviour {
 
     //Gamestates
     private bool OnWall = false;
+    private bool GrappleThrown = false;
+    private bool GrappleConnected = false;
+
+    //Grapple
+    public GameObject GrapplePrefab;
+    private GameObject Grapple;
+    private Vector3 GrappleVelocity;
 
 
     void Awake() {
@@ -40,7 +47,24 @@ public class PlayerScript : MonoBehaviour {
     void Update() {
         if (OnWall && Input.GetKeyDown(KeyCode.Space)) {
             OnWall = false;
-            rigidbody.AddForce(16f * transform.forward, ForceMode.Impulse);
+            rigidbody.AddForce(15f * transform.forward, ForceMode.Impulse);
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            GrappleThrown = true;
+            Grapple = Instantiate(GrapplePrefab, transform.position + 0.6f*transform.forward, Quaternion.Euler(transform.forward));
+            Grapple.GetComponent<Rigidbody>().linearVelocity = rigidbody.linearVelocity + 30f*transform.forward;
+            Grapple.transform.rotation = Quaternion.LookRotation(transform.forward);
+            GrappleVelocity = Grapple.GetComponent<Rigidbody>().linearVelocity;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            GrappleThrown = false;
+            GrappleConnected = false;
+            GrappleVelocity = new Vector3(999,999,999);
+            Destroy(Grapple);
+        }
+        if (!GrappleConnected && GrappleThrown && GrappleVelocity != Grapple.GetComponent<Rigidbody>().linearVelocity) {
+            Grapple.GetComponent<Rigidbody>().linearVelocity = new Vector3(0,0,0);
+            GrappleConnected = true;
         }
     }
 
@@ -66,6 +90,10 @@ public class PlayerScript : MonoBehaviour {
 
         if (!OnWall) {
             rigidbody.AddForce(4f * JetpackForce.normalized, ForceMode.Force);
+        }
+        if (GrappleConnected) {
+            rigidbody.AddForce(15f*(Grapple.transform.position - transform.position).normalized, ForceMode.Force);
+            OnWall = false;
         }
     }
 
