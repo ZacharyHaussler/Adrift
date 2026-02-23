85652e1d2f7bbdfcd01154eee525f8627b24a804
Zachary Haussler <zach.hauss@gmail.com>
2026-02-23 10:38:23 -0600

    -New mediocre map + pushing off walls

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,28 @@
-using Unity.Mathematics;
+//using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 //using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour {
 
+    //Physics
     public Rigidbody rigidbody;
+
+    //Mouse Control
     public float XSensitivity = 100f;
     public float YSensitivity = 100f;
-
     private float XRotation;
     private float YRotation;
-    
+
+    //Player Movement
+    private float x = 0.0f;
+    private float y = 0.0f;
+    private float z = 0.0f;
     private Vector3 ThrustDirection = new Vector3(0f,0f,0f);
     private float AngleForce = 0.0f;
+
+    //Gamestates
+    private bool OnWall = false;
 
 
     void Awake() {
@@ -38,16 +48,27 @@ public class PlayerScript : MonoBehaviour {
 
     void FixedUpdate(){
 
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
-        float y = (Input.GetKey(KeyCode.Space) ? 1f : 0f) - (Input.GetKey(KeyCode.LeftShift) ? 1f : 0f);
-
-        Vector3 worldForce =
+        
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+        y = (Input.GetKey(KeyCode.Space) ? 1f : 0f) - (Input.GetKey(KeyCode.LeftShift) ? 1f : 0f);
+        
+        Vector3 JetpackForce =
             x * transform.right +
             y * transform.up +
             z * transform.forward;
 
-        rigidbody.AddForce(4f * worldForce.normalized, ForceMode.Force);
+        if (!OnWall) {
+            rigidbody.AddForce(4f * JetpackForce.normalized, ForceMode.Force);
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
+            OnWall = false;
+            rigidbody.AddForce(10f * transform.forward, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        OnWall = true;
+        rigidbody.linearVelocity = new Vector3(0,0,0);
     }
 
 }
